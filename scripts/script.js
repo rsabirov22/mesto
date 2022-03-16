61c34f9670cb57fa5dace30e6547f1ecdc3195df
Ruslan Sabirov <rsabirov22@yandex.com>
2022-03-16 15:14:19 +0600

    refactor: выполнить требования ревьюера

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,12 @@
 let container = document.querySelector('.content');
-let editButton = container.querySelector('.profile-info__edit-button');
-let profileName = container.querySelector('.profile-info__name');
-let profileJob = container.querySelector('.profile-info__description');
+let editButton = container.querySelector('.profile__edit-button');
+let profileName = container.querySelector('.profile__name');
+let profileJob = container.querySelector('.profile__description');
 let popup = document.querySelector('.popup');
 let closeButton = popup.querySelector('.popup__close-btn');
 let nameInput = popup.querySelector('#nickname');
 let jobInput = popup.querySelector('#job');
-let formElement = popup.querySelector('.form__wrapper');
-
+let formElement = popup.querySelector('.form');
 
 function openPopup() {
   popup.classList.add('popup_opened');
@@ -20,16 +19,15 @@ function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-
 function formSubmitHandler (evt) {
   evt.preventDefault();
 
-  profileName.textContent = evt.target[1].value;
-  profileJob.textContent = evt.target[3].value;
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
 
-  popup.classList.remove('popup_opened');
+  closePopup();
 }
 
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler); 
